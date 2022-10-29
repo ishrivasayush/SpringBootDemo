6918cc07169aa53f58488f9373c9e7f410e7ccea
Ayush Srivastava <ayusrivastava2k3@gmail.com>
2022-10-29 22:42:17 +0530

    first commit

---
+++ src/main/java/com/Narainox/SpringbootTutoial/SpringbootTutoialApplication.java
@@ -0,0 +1,13 @@
+package com.Narainox.SpringbootTutoial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutoialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutoialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Narainox/SpringbootTutoial/SpringbootTutoialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Narainox.SpringbootTutoial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTutoialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
